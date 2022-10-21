7fd30db94a41a100e91a394de0534669dc070588
Corbin-McCallum <114627378+Corbin-McCallum@users.noreply.github.com>
2022-10-21 17:26:58 -0400

    Update vga_fsm.vhd

--- Project 2/vga_fsm.vhd
+++ Project 2/vga_fsm.vhd
@@ -16,16 +16,35 @@ entity vga_fsm is
 		point_valid:	out	boolean;
 
 		h_sync:			out	std_logic;
-		v_sync:			out std_logic
+		v_sync:			out 	std_logic
 	);
 end entity vga_fsm;
 
 architecture fsm of vga_fsm is
-	-- any internal signals you may need
+	signal reset:				std_logic;
+	signal vga_clock: 		std_logic;
 	
+	signal hsync:				std_logic;
+	signal vsync: 				std_logic;
+	signal point_valid: 		std_logic_vector(1 downto 0);
 begin
-	-- implement methodology to drive outputs here
-	-- use vga_data functions and types to make your life easier
+	sync_point: process(vga_clock,coordinate) is
+	begin
+		if vga_clock = '1' then 
+			h_sync <= do_horizontal_sync(coordinate);
+			v_sync <= do_vertical_sync(coordinate);
+		else
+			point <= next_coordinate(coordinate)
+		end if;
+	end process sync_point;
 	
+	check_point: process(coordinate) is
+	begin
+		if point_visible(coordinatecoordinate) = '1' then 
+			point_valid <= 1;
+		else
+			point_valid <= 0;
+		end if;
+	end process check_point;
 
 end architecture fsm;

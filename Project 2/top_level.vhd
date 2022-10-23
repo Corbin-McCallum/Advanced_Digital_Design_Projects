cd01767392e4e505a1f3bf217dcfdec56271d0de
Corbin-McCallum <114627378+Corbin-McCallum@users.noreply.github.com>
2022-10-23 16:51:27 -0400

    Update top_level.vhd

--- Project 2/top_level.vhd
+++ Project 2/top_level.vhd
@@ -2,11 +2,18 @@ library ieee;
 use ieee.std_logic_1164.all;
 
 entity top_level is
-	
+	generic(
+		lines:	natural range 0 to 479;
+		pixels: 	natural range 0 to 639
+	);
+	port (
+		reset:		in	std_logic;
+		clock:		in	std_logic
+	);
 end entity top_level;
 
 architecture arch1 of top_level is
 
 begin
 
-end architecture top_level;
+end architecture arch1;

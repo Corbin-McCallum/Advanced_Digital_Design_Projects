53bc8923d17ca36b53460a37b22f493019a664f7
Corbin-McCallum <114627378+Corbin-McCallum@users.noreply.github.com>
2022-10-23 16:47:48 -0400

    Update control_unit.vhd

--- Project 2/control_unit.vhd
+++ Project 2/control_unit.vhd
@@ -2,16 +2,22 @@ library ieee;
 use ieee.std_logic_1164.all;
 
 entity control_unit is
-	-- Input ports
-	done: 			in 	std_logic;
-	-- Output ports
-	FPGA_clock:		out	std_logic;
-	reset:			out	std_logic;
-	wren:			out	std_logic
+	generic(
+		lines:	natural 	range 	0 to 479;
+		pixels: natural 	range 	0 to 639;
+	);
+	port (
+		-- Input ports
+		done: 			in 	std_logic;
+		-- Output ports
+		FPGA_clock:		out	std_logic;
+		reset:			out	std_logic;
+		wren:			out	std_logic
+	);
 end entity control_unit;
 
 architecture logic of control_unit is
-
+	signal current_point: corrdiante;
 begin
 
 end architecture logic;

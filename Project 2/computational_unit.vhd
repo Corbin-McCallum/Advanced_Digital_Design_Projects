2ee5628c324461c0f4c871928e62b37776a85318
Victor-Omenya <89947965+Victor-Omenya@users.noreply.github.com>
2022-10-24 18:32:34 -0400

    Update computational_unit.vhd

--- Project 2/computational_unit.vhd
+++ Project 2/computational_unit.vhd
@@ -3,56 +3,50 @@ use ieee.std_logic_1164.all;
 
 entity computational_unit is
 	generic(
-		lines:		natural range 0 to 479;
-		pixels: 	natural range 0 to 639
+		iterations: positive range 1 to 64:= 32;
+		threshold:	ads_sfixed := to_ads_sfixed(4);
 	);
 	port (
 		-- Input ports
 		fpga_clock: 			in 	std_logic;
-		reset:				in	std_logic;
+		reset:					in		std_logic;
+		seed:						in		ads_complex; --complex #C
 		-- Output ports
-		done:				out	std_logic;
-		iteration_count:		out	std_logic
+		done:						out	std_logic;
+		iteration_count:		out	natural range 0 to iterations - 1;
 	);
 end entity computational_unit;
 
 architecture computation of computational_unit is
-	signal threshold:	natural;
-	signal c: 		ads_complex;
-	signal z: 		ads_complex;
-	signal iteration: 	natural;
-	signal iterations:	natural;
+	-- signal threshold:		natural;
+	-- signal c: 				ads_complex;
+	signal z: 				ads_complex;
+	signal iteration: 	natural range 0 to iterations - 1;
+	signal all_done:	std_logic;
+	-- signal iterations:	natural;
 begin
 	-- Obtaining a colored point on the Mandelbrot set
-	compute_point: process(c,iterations)
-		z			<= 0;
-		iteration		<= 0;
-		c 			<= ads_cmplx(-0.5,0.5);
-		threshold		<= 2;
-
-		algo_1: while iteration < iterations loop
-			z <= ads_square(z);
-			Z <= z + c;
-			
-			if abs2(z) > threshold then
-				exit algo_1;
+	compute_point: process(reset, fpga_clock) is
+	begin
+		if reset = '0';
+			z <= ( re => (others => '0'), im => (others => '0') );
+			iteration <= 0;
+			all_done <= '0';
+		elsif rising_edge(fpga_clock) then
+			if all_done = '0' then
+				z <= ads_square(z) + seed;
+				iteration <= iteration + 1;
+				if abs2(z) >= threshold or iteration = iterations - 1 then
+					all_done <= '1';
+				-- else
+				--	done <= '0';
+				end if;
 			end if;
-			iteration <= iteration + 1;
-		end loop algo_1;
-		
-		port map(
-			iteration_count <= iteration;
-		);
+		end if;
 	end process compute_point;
 	
-	-- Generating the colored Mandelbrot set
-	generate_set: process(c)
-		line_count: for i in lines'range generate
-			pixel_count: for j in pixels'range generate
-				color <= compute_point(c);
-				plot i,j, color;
-			end generate pixel_count;		
-		end generate line_count;
-	end process generate_set;
+	iteration_count <= iteration;
+	done <= all_done;
 	
+
 end architecture computation;

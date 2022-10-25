10d955ddbf93d971474e47e5902cd26833b32bad
Victor-Omenya <89947965+Victor-Omenya@users.noreply.github.com>
2022-10-25 17:00:22 -0400

    Update control_unit.vhd

--- Project 2/control_unit.vhd
+++ Project 2/control_unit.vhd
@@ -4,17 +4,18 @@ use ieee.std_logic_1164.all;
 entity control_unit is
 	generic(
 		threshold:	ads_sfixed := to_ads_sfixed(4);
-		lines:		natural range 0 to 479;
-		pixels: 	natural range 0 to 639
+		lines:	natural range 0 to 479;
+		pixels: 	natural range 0 to 479
 	);
 	port (
 		-- Input ports
-		done: 			in 	std_logic;
-		iteration_count: 	in	std_logic;
+		done: 				in 	std_logic;
+		iteration_count: 	in		std_logic;
+		seed_limit:			in		std_logic;
 		-- Output ports
 		FPGA_clock:		out	std_logic;
 		reset:			out	std_logic;
-		wren:			out 	std_logic
+		wren:				out 	std_logic;
 	);
 end entity control_unit;
 
@@ -23,7 +24,7 @@ architecture logic of control_unit is
 	type state_type is (reset_state, generate_next_seed, enable, store_result, done_state);
 	signal state, next_state: state_type := reset;
 	
-	signal current_point: corrdiante;
+	signal current_point: coordinate;
 	signal seed: ads_complex;
 begin
 
@@ -31,22 +32,32 @@ begin
 		begin
 			case state is
 				when reset_state => 
-					-- if reset = '0' then
+					-- set reset = '0' then next state
+						reset <= '0';
 						next_state <= generate_next_seed;
-					--else
-						--next_state <= done_state;
-					--end if;
-				when generate_next_seed => next_state <= enable;
+					
+				when generate_next_seed => 
+					--check the seed limit first
+					if (seed_limit = '0') then --haven't reached the seed limit
+						next_state <= enable;
+					else 
+						next_state <= store_result;
+					end if;
 				when enable => 
+					--check if computational unit is all done with iterations
 					if done = '1'then
 						next_state <= store_result;
 					else
+						--do something with fpga clock
+						fpga_clock <= '1';
 						next_state <= enable;
 					end if;
 				when store_result =>
-					if (LAST_SEED_CONDITION) then
+					--if at the seed limit, set wren to 1 for ram to start storing computational data
+					if (seed_limit = '1') then
+						wren <= '1';
 						next_state <= done_state;
-					else
+					else --the seed limit hasn't been reached, next state is reset.
 						next_state <= reset_state;
 					end if;
 				when done_state => next_state <= done_state;

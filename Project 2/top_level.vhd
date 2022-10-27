58256e9a957b4cc321b2b88e3aaf3e8d2d4c60e9
Kalvin McCallum <114109316+kalvinmccallum@users.noreply.github.com>
2022-10-27 19:48:50 -0400

    Update top_level.vhd

--- Project 2/top_level.vhd
+++ Project 2/top_level.vhd
@@ -4,6 +4,12 @@ use ieee.std_logic_1164.all;
 library work;
 use work.project_pkg.all;
 
+library vga;
+use vga.vga_data.all;
+
+library ads;
+use ads.ads_fixed.all;
+
 entity top_level is
 	generic(
 		lines:		natural range 0 to 479;
@@ -16,58 +22,73 @@ entity top_level is
 end entity top_level;
 
 architecture arch1 of top_level is
-
+	constant threshold: ads_sfixed := to_ads_sfixed(4);
+	constant iterations: positive range 1 to 64:= 32;
+	constant stages: natural := 3;
+	signal clock_signal: std_logic;
+	signal reset_signal: std_logic;
+	signal wren_signal: std_logic;
+	signal seed_signal: ads_complex;
+	signal iteration_count_signal_1: natural range 0 to iterations - 1;
+	signal iteration_count_signal_2: std_logic;
+	signal done_signal: std_logic;
+	signal point_signal:	std_logic;
+	signal point_valid_signal:	boolean;
+	signal v_sync_signal:	std_logic;
+	signal h_sync_signal:	std_logic;
 begin
 	
 	signal_driver:vga_fsm
 		port map (
 			-- Input ports
-			FPGA_clock 		=> clock;
-			reset 			=> reset;
+			FPGA_clock 		=> clock_signal,
+			reset 			=> reset_signal,
 			-- Output ports
-			point			=> point;
-			point_valid		=> point_valid;
-			h_sync			=> h_sync;
-			v_sync			=> v_sync
+			point			=> point_signal,
+			point_valid		=> point_valid_signal,
+			h_sync			=> h_sync_signal,
+			v_sync			=> v_sync_signal
 		);
 
 	control:control_unit
 		generic map(
-			threshold 		=> threshold;
-			lines			=> lines;
+			threshold 		=> threshold,
+			lines			=> lines,
 			pixels 			=> pixels
-		);
+		)		
 		port map(
 			-- Input ports
-			done			=> done;
-			iteration_count		=> iteration_count;
+			done			=> done_signal,
+			iteration_count		=> iteration_count_signal_2,
 			-- Output ports
-			FPGA_clock		=> clock;
-			reset			=> reset;
-			wren			=> wren
+			FPGA_clock		=> clock_signal,
+			reset			=> reset_signal,
+			wren			=> wren_signal
 		);
+		
 	mandalbrot_engine:computational_unit
 		generic map(
-			iterations		=> iterations;
+			iterations		=> iterations,
 			threshold		=> threshold
-		);
+		)
 		port map(
 			-- Input ports
-			fpga_clock		=> clock;
-			reset			=> reset;
-			seed			=> seed; --complex #C
+			fpga_clock		=> clock_signal,
+			reset			=> reset_signal,
+			seed			=> seed_signal, --complex #C
 			-- Output ports
-			done			=> done;
-			iteration_count		=> iteration_count
-		);.
+			done			=> done_signal,
+			iteration_count		=> iteration_count_signal_1
+		);
+		
 	sync:synchronizer
 		generic map(
 			stages			=> stages
-		);
-		port (
-			clock			=> clock;
-			reset			=> reset;
-			data_in			=> data_in;
+		)
+		port map(
+			clock			=> clock_signal,
+			reset			=> reset_signal,
+			data_in			=> point_signal,
 			data_out		=> data_out
 		);
 end architecture arch1;

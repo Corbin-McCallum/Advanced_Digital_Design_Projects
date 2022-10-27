decad0aa2a6481f04650938fa9837c773a6a9199
Corbin-McCallum <114627378+Corbin-McCallum@users.noreply.github.com>
2022-10-27 16:28:34 -0400

    Add files via upload

---
+++ Project 2/project_pkg.vhd
@@ -0,0 +1,39 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+package project_pkg is
+	---- component declarations
+	component vga_fsm is
+		generic (
+			vga_res:	vga_timing := vga_res_default
+		);
+		port (
+			-- Input ports
+			FPGA_clock:		in	std_logic;
+			reset:			in	std_logic;
+			-- Output ports
+			point:			out	coordinate;
+			point_valid:	out	boolean;
+			h_sync:			out	std_logic;
+			v_sync:			out 	std_logic
+		);
+	end component vga_fsm;
+	
+	component control_unit is
+		generic(
+			threshold:	ads_sfixed := to_ads_sfixed(4);
+			lines:	natural range 0 to 479;
+			pixels: 	natural range 0 to 639
+		);
+		port (
+			-- Input ports
+			done: 				in 	std_logic;
+			iteration_count: 	in		std_logic;
+			-- Output ports
+			FPGA_clock:			out	std_logic;
+			reset:				out	std_logic;
+			wren:					out std_logic
+		);
+	end component control_unit;
+	
+end package project_pkg;
--- Project 2/top_level.vhd
+++ Project 2/top_level.vhd
@@ -1,10 +1,13 @@
 library ieee;
 use ieee.std_logic_1164.all;
 
+library work;
+use work.project_pkg.all;
+
 entity top_level is
 	generic(
 		lines:	natural range 0 to 479;
-		pixels: natural range 0 to 639
+		pixels: 	natural range 0 to 639
 	);
 	port (
 		reset:		in	std_logic;
@@ -15,5 +18,33 @@ end entity top_level;
 architecture arch1 of top_level is
 
 begin
+	
+	signal_driver: vga_fsm
+	port map (
+		-- Input ports
+		FPGA_clock 		=> clock;
+		reset 			=> reset
+		-- Output ports
+		point				=> ;
+		point_valid		=> ;
+		h_sync			=> ;
+		v_sync			=>
+	);
+
+	control:control_unit
+	generic map(
+		threshold 	=> threshold;
+		lines			=> ;
+		pixels 		=>
+	);
+	port map(
+		-- Input ports
+		done					=> ;
+		iteration_count	=> ;
+		-- Output ports
+		FPGA_clock			=> ;
+		reset					=> ;
+		wren					=>
+	);
 
 end architecture arch1;

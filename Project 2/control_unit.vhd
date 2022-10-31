1ca5ea043f7f6f0e788ef3c335ed4c32e4169350
Corbin-McCallum <114627378+Corbin-McCallum@users.noreply.github.com>
2022-10-31 14:36:07 -0400

    Update control_unit.vhd

--- Project 2/control_unit.vhd
+++ Project 2/control_unit.vhd
@@ -9,7 +9,7 @@ entity control_unit is
 	port (
 		-- Input ports
 		reset:			in	std_logic;
-		FPGA_clock:		in	std_logic;
+		fpga_clock:		in	std_logic;
 		-- Output ports
 		address:		out 	natural;
 		iterations:		out 	natural;
@@ -21,9 +21,10 @@ end entity control_unit;
 architecture logic of control_unit is
 	-- Type and signal decleration used for the control_unit
 	type state_type is (reset_state, generate_next_seed, enable, store_result, done_state);
-	signal computation_done: 
+	signal computation_done: std_logic;
 	signal state, next_state: state_type := reset;
 	signal current_point: coordinate;
+	signal enable_computation: std_logic;
 	signal seed: ads_complex;
 begin
 
@@ -43,7 +44,7 @@ begin
 					end if;
 				when store_result =>
 					--if at the seed limit, set wren to 1 for ram to start storing computational data
-					if (current_point.x = '479' and current_point.y = '479') then
+					if (current_point.x = 479 and current_point.y = 479) then
 						next_state <= done_state;
 					else --the seed limit hasn't been reached, next state is reset.
 						next_state <= reset_state;
@@ -53,32 +54,45 @@ begin
 			end case;
 	end process transition_function;
 	
-	output_process: process(state) is
+	save_state: process(fpga_clock) is
 		begin
-			if state = reset_state then
-				reset <= '0';
-			else
-				reset <= '1';
+			-- check for edge of clock, add reset logic
+			if rising_edge(fpga_clock) then
+				if reset = '0' then
+					state <= reset_state;
+				else
+					state <= next_state;
+				end if;
 			end if;
-			
-			if state = generate_next_seed then
-					
-			else
+	end process save_state;
 				
+	output_process: process(fpga_clock) is
+		begin -- add reset logic
+			if rising_edge(fpga_clock) then
+				if state = reset_state then
+					reset <= '0';
+				else
+					reset <= '1';
+				end if;
+
+				if state = generate_next_seed then
+					current_point <= ---
+				else
+					current_poimt <= current_point;
+				end if;
+
+				if state = enable then
+					enable_computation <= '1';
+				else
+					enable_computation <= '0';
+				end if;
+
+				if state = store_result then
+					wren <= '1';
+				else
+					wren <= '0';
+				end if;
 			end if;
-			
-			if state = enable then
-			
-			else
-					
-			end if;
-			
-			if state = store_result then
-			
-			else
-				
-			end if;
-			
 	end process output_process;
 end architecture logic; 
 

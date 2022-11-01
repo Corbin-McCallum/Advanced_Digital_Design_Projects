de7e56d31a5a0382d3afcca31c1df0d8fe4458b9
Corbin-McCallum <114627378+Corbin-McCallum@users.noreply.github.com>
2022-10-31 21:11:09 -0400

    Update mandelbrot_netpbm_generator.vhd

--- Project 2/mandelbrot_netpbm_generator.vhd
+++ Project 2/mandelbrot_netpbm_generator.vhd
@@ -1,10 +1,12 @@
 library ieee;
 use ieee.std_logic_1164.all;
-use ieee.fixed_pkg.all;
 
 use std.textio.all;
 
-use work.fixed_complex.all;
+library ads;
+use ads.ads_complex_pkg.all;
+use ads.ads_fixed.all;
+
 use work.netpbm_config.all;
 
 entity mandelbrot_netpbm_generator is
@@ -12,30 +14,27 @@ end entity mandelbrot_netpbm_generator;
 
 architecture test_fixture of mandelbrot_netpbm_generator is
 
-	component mandelbrot_pipeline is
-		generic (
-			iterations:	natural range 8 to 64		:= 32;
-			int_bits:	natural range 3 to 12		:= 4;
-			fract_bits:	natural range 6 to 36		:= 10;
-			escape:		sfixed	:= to_sfixed(4, int_bits, -fract_bits)
+	-- your mandelbrot computational engine here
+	component computational_unit is
+		generic(
+			iterations: positive range 1 to 64:= 32;
+			threshold:	ads_sfixed := to_ads_sfixed(4)
 		);
 		port (
-			enable:				in	std_logic;
-			clock:				in	std_logic;
-			reset:				in	std_logic;
-			seed:				in	fcomplex(re(int_bits downto -fract_bits),
-										im(int_bits downto -fract_bits));
-	
-			iteration_count:	out	natural range 0 to iterations - 1;
-			output_valid:		out	boolean
+			-- Input ports
+			fpga_clock: 			in 	std_logic;
+			reset:					in		std_logic;
+			seed:						in		ads_complex; --complex #C
+			-- Output ports
+			done:						out	std_logic;
+			iteration_count:		out	natural range 0 to iterations - 1
 		);
-	end component mandelbrot_pipeline;
+	end entity computational_unit;
 
 
 	signal iteration_test: natural range 0 to iterations + 1;
 
-	signal seed: fcomplex(re(int_bits downto -fract_bits),
-					im(int_bits downto -fract_bits));
+	signal seed: ads_complex;
 	signal clock: std_logic		:= '0';
 	signal reset: std_logic		:= '0';
 	signal enable: std_logic	:= '0';
@@ -49,26 +48,22 @@ begin
 
 	clock <= not clock after 1 ps when not finished else '0';
 
-	generator: mandelbrot_pipeline
+	generator:computational_unit
 		generic map (
-			iterations => iterations,
-			int_bits => int_bits,
-			fract_bits => fract_bits,
-			escape => escape
+			iterations 			=> iterations,
+			threshold			=> threshold
 		)
 		port map (
-			enable => enable,
-			clock => clock,
-			reset => reset,
-			seed => seed,
-
-			iteration_count => iteration_count,
-			output_valid => output_valid
+			fpga_clock			=> clock,
+			reset 				=> reset,
+			seed 					=> seed,
+			done					=> done,
+			iteration_count 	=> iteration_count
 		);
 	
 	make_pgm: process
-		variable x_coord: sfixed(seed.re'range);
-		variable y_coord: sfixed(seed.re'range);
+		variable x_coord: ads_sfixed;
+		variable y_coord: ads_sfixed;
 		variable output_line: line;
 	begin
 		-- header information
@@ -83,6 +78,8 @@ begin
 		write(output_line, integer'image(iterations - 1));
 		writeline(output, output_line);
 
+		-- from here onwards, stimulus depends on your implementation
+
 		-- ensure generator is disabled
 		enable <= '0';
 
@@ -92,26 +89,8 @@ begin
 		wait until rising_edge(clock);
 		reset <= '0';
 
+		-- enable the generator
 		enable <= '1';
-
-		for y_pt in 0 to y_steps-1 loop
-			y_coord := resize(y_range.min + y_pt * dy,
-						y_coord);
-			for x_pt in 0 to x_steps-1 loop
-
-				x_coord := resize(x_range.min + x_pt * dx, x_coord);
-
-				-- set seed
-				seed <= fcmplx(x_coord, y_coord);
-				wait until rising_edge(clock);
-
-				if output_valid then
-					write(output_line, integer'image(iterations - 1 - iteration_count));
-					writeline(output, output_line);
-					flush(output);
-				end if;
-			end loop;
-		end loop;
 
 		for i in 0 to iterations - 1 loop
 			wait until rising_edge(clock);

4939d1a92b0cf1c8e98dc3dc889ffcf4b3f94e7e
Victor-Omenya <89947965+Victor-Omenya@users.noreply.github.com>
2022-11-02 16:02:24 -0400

    Update netpbm_config.vhd

--- Project 2/netpbm_config.vhd
+++ Project 2/netpbm_config.vhd
@@ -1,13 +1,14 @@
-library ieee;
-use ieee.fixed_pkg.all;
-use ieee.fixed_float_types.all;
+llibrary ieee;
+
+library ads;
+use ads.ads_fixed.all;
 
 package netpbm_config is
 	-- record for minimum and maximum
 	type plot_range is
 	record
-		min:	sfixed;
-		max:	sfixed;
+		min:	real;
+		max:	real;
 	end record plot_range;
 
 	-- test fixture parameters
@@ -15,28 +16,24 @@ package netpbm_config is
 	constant fract_bits: natural range 6 to 18 := 18;
 
 	constant iterations: natural := 32;
-	constant escape: sfixed := to_sfixed(32, int_bits, -fract_bits);
+	constant escape: ads_sfixed := to_ads_sfixed(4);
 
 	constant x_range: plot_range := (
-			min => to_sfixed(-2.2, int_bits, -fract_bits),
-			max => to_sfixed(1, int_bits, -fract_bits)
+			min => -2.2,
+			max =>  1.0
 		);
 	constant y_range: plot_range := (
-			min => to_sfixed(-1.2, int_bits, -fract_bits),
-			max => to_sfixed(1.2, int_bits, -fract_bits)
+			min => -1.2,
+			max =>  1.2
 		);
 
 	constant y_steps: natural range 10 to natural'high := 480;
-	constant x_steps: natural range 10 to natural'high := 640;
+	constant x_steps: natural range 10 to natural'high := 480;
 
 	-- other stuff
-	constant dy: sfixed := resize((y_range.max - y_range.min) / y_steps,
-										int_bits, -fract_bits,
-									overflow_style => fixed_saturate,
-									round_style => fixed_truncate);
-	constant dx: sfixed := resize((x_range.max - x_range.min) / x_steps,
-										int_bits, -fract_bits,
-									overflow_style => fixed_saturate,
-									round_style => fixed_truncate);
+	constant dy: ads_sfixed := to_ads_sfixed((y_range.max - y_range.min)
+									/ real(y_steps));
+	constant dx: ads_sfixed := to_ads_sfixed((x_range.max - x_range.min)
+									/ real(x_steps));
 
 end package netpbm_config;

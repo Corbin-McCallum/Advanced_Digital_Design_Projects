a504bd7fe738a6fe271edbddd007da911b7c4f18
Victor-Omenya <89947965+Victor-Omenya@users.noreply.github.com>
2022-11-29 11:48:31 -0500

    Update toplevel.vhd

--- toplevel.vhd
+++ toplevel.vhd
@@ -12,17 +12,19 @@ entity toplevel is
 		clock_50:	in	std_logic;
 		reset:		in	std_logic;
 		
-		hex_digits:	out seven_segment_digit_array
+		hex_digits:	out 	seven_segment_digit_array
 	);
 end entity toplevel;
 
 architecture top of toplevel is
-	signal counter: unsigned(29 downto 0);
+	signal counter: 	unsigned(29 downto 0);
+	signal binary_out: 	std_logic;
+	signal binary_in: 	std_logic;
 begin
 
 	assign_digit: for digit in seven_segment_digit_array'range generate
 		constant high_bit: natural := counter'high - 4*(5 - digit);
-		constant low_bit: natural := high_bit - 3;
+		constant low_bit:  natural := high_bit - 3;
 	begin
 		hex_digits(digit) <= get_hex_digit(to_integer(counter(high_bit downto low_bit)), lamp_mode);
 	end generate;
@@ -35,5 +37,86 @@ begin
 			counter <= counter + "1";
 		end if;
 	end process count;
+	
+	ma0: max10_adc
+		port map (
+			pll_clk 		=> clock_10,
+			chsel   		=> '0',
+			soc	  		=> start_conversion,
+			tsen	  		=> '1' ,
+			dout	  		=> data_out,
+			eoc	  		=> end_conversion,
+			clk_dft 		=> clock_1
+		);
+	
+	ac0: adc_control
+		generic map(
+			max_address 		=>
+		);
+		port map (
+			clock_10 		=> clock_10,
+			clock_1  		=> clock_1,
+			tail_ptr 		=> ,
+			reset			=> ,
+		
+			write_en 		=> ,
+			data_out 		=> ,
+			head_ptr 		=>
+		);
+		
+	dc0: display_control
+		generic map(
+			max_address =>
+		);
+		port map(
+			-- Input
+			clock_50		=> clock_50 ,
+			head_ptr		=> ,
+			reset			=> ,
+			-- Output
+			write_en		=> ,
+			tail_ptr		=> ,
+			data_out		=> 
+		);
+	
+	b0: buffer
+		generic map
+		(
+			DATA_WIDTH 		=> ,
+			ADDR_WIDTH 		=> 
+		);
+
+		port map
+		(
+			clk_a			=> clock_1,
+			clk_b			=> clock_50,
+			addr_a			=> ,
+			addr_b			=> ,
+			data_a			=> ,
+			data_b			=> ,
+			we_a			=> ,
+			we_b			=> ,
+			q_a			=> ,
+			q_b			=> 
+		);
+		
+	gtb0: gray_to_binary
+		generic map(
+			input_width		=>
+		);
+		port map(
+			gray_in			=> ,
+			bin_out			=> binary_out
+		);
+		
+	btg0: binary_to_gray
+		generic map(
+			input_width		=>
+		);
+		port map(
+			bin_in			=> binary_in,
+			gray_out		=>
+		);
+	
 
 end architecture top;

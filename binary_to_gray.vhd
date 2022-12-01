3081490d9633504a3f508e9ba1730e7f885575e7
Victor-Omenya <89947965+Victor-Omenya@users.noreply.github.com>
2022-12-01 17:57:08 -0500

    Update binary_to_gray.vhd

--- binary_to_gray.vhd
+++ binary_to_gray.vhd
@@ -1,17 +1,17 @@
 library ieee;
 use ieee.std_logic_1164.all;
 
-entity bin_to_gray is
+entity binary_to_gray is
 	generic (
 		input_width: positive := 16
 	);
 	port (
-		bin_in: in std_logic_vector(input_width - 1 downto 0);
-		gray_out: out std_logic_vector(input_width - 1 downto 0)
+		bin_in: 	in std_logic_vector(input_width - 1 downto 0);
+		gray_out: 	out std_logic_vector(input_width - 1 downto 0)
 	);
-end entity bin_to_gray;
+end entity binary_to_gray;
 
-architecture rtl of bin_to_gray is
+architecture rtl of binary_to_gray is
 	signal shifted: std_logic_vector(bin_in'range);
 	
 	begin

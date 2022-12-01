ff7ac20c15c5cf4d2ca8074bd6ca62c872c3da11
Victor-Omenya <89947965+Victor-Omenya@users.noreply.github.com>
2022-12-01 18:02:52 -0500

    Update toplevel.vhd

--- toplevel.vhd
+++ toplevel.vhd
@@ -3,6 +3,7 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 use work.seven_segment_pkg.all;
+use work.project_pkg.all;
 
 entity toplevel is
 	generic (
@@ -10,11 +11,11 @@ entity toplevel is
 		address_width: natural := 6
 	);
 	port (
-		clock_50:	in	std_logic;
-		clock_10:	in 	std_logic;
-		reset:		in	std_logic;
+		clock_50:	in  std_logic;
+		clock_10:	in  std_logic;
+		reset:		in  std_logic;
 		
-		hex_digits:	out 	seven_segment_digit_array
+		hex_digits:	out seven_segment_digit_array
 	);
 end entity toplevel;
 
@@ -27,15 +28,21 @@ architecture top of toplevel is
 	-- signal binary_in: 			std_logic;
 	-- signal input_width_signal: positive := 16;
 	-- signal gray_in_signal: 		std_logic_vector(input_width - 1 downto 0);
+	signal raw_hex_data: std_logic_vector(23 downto 0);
+	signal clock_1: std_logic;
+	signal write_en: std_logic;
+	signal data_out: natural range 0 to 2**12 - 1;
 	
+	signal head_ptr_50_v: std_logic_vector(address_width - 1 downto 0);
+	signal tail_ptr_1_v: std_logic_vector(address_width - 1 downto 0);
 	
 begin
 
 	assign_digit: for digit in seven_segment_digit_array'range generate
-		constant high_bit: natural := counter'high - 4*(5 - digit);
-		constant low_bit:  natural := high_bit - 3;
+		constant high_bit: natural := 4 * digit + 3;
+		constant low_bit:  natural := 4 * digit;
 	begin
-		hex_digits(digit) <= get_hex_digit(to_integer(counter(high_bit downto low_bit)), lamp_mode);
+		hex_digits(digit) <= get_hex_digit(to_integer(unsigned(raw_hex_data(high_bit downto low_bit))), lamp_mode);
 	end generate;
 	
 	
@@ -47,7 +54,7 @@ begin
 			clock_10 	=> clock_10,
 			clock_1  	=> clock_1,
 			tail_ptr 	=> tail_ptr_1,
-			reset	 	=> reset,
+			reset		=> reset,
 		
 			write_en 	=> write_en,
 			data_out 	=> data_out,
@@ -56,7 +63,7 @@ begin
 		
 	dc0: display_control
 		generic map(
-			max_address => max_address
+			max_address 	=> max_address
 		)
 		port map(
 			-- Input
@@ -70,45 +77,47 @@ begin
 	b0: true_dual_port_ram_dual_clock
 		generic map
 		(
-			DATA_WIDTH 	=> address_width,
-			ADDR_WIDTH 	=> addr_width_signal
+			DATA_WIDTH 	=> 12,
+			ADDR_WIDTH 	=> address_width
 		)
-
 		port map
 		(
 			clk_a		=> clock_1,
 			clk_b		=> clock_50,
 			addr_a		=> head_ptr_1,
 			addr_b		=> tail_ptr_50,
-			data_a		=> data_out,
+			data_a		=> std_logic_vector(to_unsigned(data_out, 12)),
 			data_b		=> (others => '0'),
 			we_a		=> write_en,
 			we_b		=> '0',
 			q_a		=> open,
-			q_b		=> data_out
+			q_b		=> raw_hex_data(11 downto 0)
 		);
+	raw_hex_data(23 downto 12) <= (others => '0');
 		
 	tss0: two_stage_synchronizer
 		generic map(
-			input_width	=> input_width
+			input_width	=> address_width
 		)
 		port map(
-			data_in		=> head_ptr_1,
+			data_in		=> std_logic_vector(to_unsigned(head_ptr_1, address_width)),
 			clk_1		=> clock_1,
 			clk_2		=> clock_50,
-			data_out	=> head_ptr_50
+			data_out	=> head_ptr_50_v
 		);
+	head_ptr_50 <= to_integer(unsigned(head_ptr_50_v));
 		
 	tss1: two_stage_synchronizer
 		generic map(
-			input_width	=> input_width
+			input_width	=> address_width
 		)
 		port map(
-			data_in		=> tail_ptr_50,
+			data_in		=> std_logic_vector(to_unsigned(tail_ptr_50, address_width)),
 			clk_1		=> clock_50,
 			clk_2		=> clock_1,
-			data_out	=> tail_ptr_1
+			data_out	=> tail_ptr_1_v
 		);
+	tail_ptr_1 <= to_integer(unsigned(tail_ptr_1_v));
 
 
 end architecture top;

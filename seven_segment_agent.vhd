9588d76e519fb7070a3f915c8f314e539073c412
Corbin-McCallum <114627378+Corbin-McCallum@users.noreply.github.com>
2022-12-05 18:25:27 -0500

    Update seven_segment_agent.vhd

--- seven_segment_agent.vhd
+++ seven_segment_agent.vhd
@@ -32,13 +32,13 @@ architecture logic of seven_segment_agent is
 	-- functions
 	function: concat_function(
 		config:		in	seven_segment_output;
-	) return std_logic_vector
+	) return std_logic_vector;
 	is
 		variable ret:	std_logic_vector(41 downto 0);
 	begin
 		for i in seven_segment_output'range loop
 			ret(7*i + 6 downto 7*i) := config(i).g & config(i).f & config(i).e &	config(i).d & config(i).c & config(i).b & config(i).a;
-		end loop;
+		end loop
 		return ret;
 	end function concat_function;
 begin

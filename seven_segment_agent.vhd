a7c3cd8e51c3c2ed649b8495e5785992c4551f2d
Corbin-McCallum <114627378+Corbin-McCallum@users.noreply.github.com>
2022-12-05 20:04:40 -0500

    Update seven_segment_agent.vhd

--- seven_segment_agent.vhd
+++ seven_segment_agent.vhd
@@ -6,19 +6,19 @@ use work.seven_segment_config.all;
 
 entity seven_segment_agent is
 	generic (
-		lamp_mode: 			lamp_configuration;
+		lamp_mode: 				lamp_configuration;
 		decimal_support:		boolean;
 		implementer: 			natural	:= 200;
-		revision: 			natural	:= 0;
+		revision: 				natural	:= 0;
 		signed_support: 		boolean;
 		blank_zeros_support	boolean
 	);
 	port (
 		-- Input ports
-		clk:			in	std_logic;
-		reset_n:		in	std_logic;
-		address:		in	std_logic_vector(1 downto 0);
-		read:			in	std_logic;
+		clk:				in	std_logic;
+		reset_n:			in	std_logic;
+		address:			in	std_logic_vector(1 downto 0);
+		read:				in	std_logic;
 		write:			in	std_logic;
 		writedata:		in	std_logic_vector(31 downto 0);
 		-- Output ports
@@ -45,8 +45,13 @@ architecture logic of seven_segment_agent is
 			ret(3) = '1';
 		end if;
 		
+		
 		if decimal_support then
 			ret(0) = '1';
+		end if;
+		
+		if blank_zeros_support then
+			ret(2) = '1';
 		end if;
 		return ret;
 	end procedure get_features;
